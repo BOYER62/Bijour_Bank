8aa5a57b8605572784eecf183588553c4bdcfc76
BOYER62 <boyer.olivier2@free.fr>
2022-06-22 13:47:20 +0200

    V0.002

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,75 @@
 let count =0;
 let solde =2650;
+let img = `sac-dargent.png`;
+
+let operations = [{
+  title : `Salaire`,
+  description : `mois de septembre`,
+  money : 1200,
+  percentage : 100,
+  operation : `credit`,
+},
+{
+  title : 'Loyer',
+  description : `mois d'aoüt`,
+  money : 450,
+  percentage : 37.5,
+  operation : `debit`,
+},
+{
+  title : 'Vente Boncoin',
+  description : `jeu PS5`,
+  money : 25,
+  percentage : 3.33,
+  operation : `credit`,
+}];
+  console.log(operations);
+if(operations[0].operation == 'credit'){
+  img == 'sac-dargent.png';
+};
+if(operations[0].operation == 'debit'){
+  img == 'depenses.png';
+}
+  
+const template = `<div class="operation `+ operations[0].operation +`">
+<div class="grid-x grid-padding-x align-middle">
+  <div class="cell shrink">
+    <div class="picto">
+      <img src="./assets/images/`+ img +`" alt="`+ operations[0].operation +`" />
+    </div>
+  </div>
+  <div class="cell auto">
+    <div>
+      <h2>`+ operations[0].title +`</h2>
+      <small>`+ operations[0].description +`</small>
+    </div>
+  </div>
+  <div class="cell small-3 text-right">
+    <div>
+      <p class="count">`+ operations[0].money +`€</p>
+      <small>`+ operations[0].percentage +`%</small>
+    </div>
+  </div>
+</div>
+</div>`;
+
+ 
 const btnSubmit = document.querySelector('.btSubmit');
 const donneesInput = document.querySelectorAll('#operationForm input');
 const donnees = document.querySelectorAll('#operationForm select');
-const ajoutOpperation = document.getElementById('ajoutOpperation');
+const ajouterOperation = document.getElementById('ajouterOperation');
 const submit = document.querySelector('#operationForm');
 const debit = document.querySelectorAll('.debit');
 const credit = document.querySelectorAll('.credit');
 const navHeader = document.querySelectorAll('.navHeader a');
 
 document.getElementById('solde').innerHTML = solde +' €';
+console.log(operations[0].operation);
+document.getElementById('ajouterOperation').innerHTML= template;
+console.log(template);
+
+operations.forEach((a,b) => {
+});
 
 // Affichage de toutes les operations
 
@@ -61,6 +121,7 @@ navHeader[2].addEventListener('click',function(){
 
 //Ajouter une opperation
 submit.addEventListener('click',function(){
+  ajoutOpperation.innerHTML = donneesInput[0].value + donneesInput[1].value + donneesInput[2].value;
   console.log(donneesInput[0].value);
   console.log(donneesInput[1].value);
   console.log(donneesInput[2].value);
@@ -76,6 +137,7 @@ submit.addEventListener('click',function(){
 function submitBeatport()
 {
           submit.submit();
+          submit.reset();
 }
 
 
